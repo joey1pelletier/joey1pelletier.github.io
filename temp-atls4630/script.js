b92e66535b5e1fa6cd951e9c6fb55cdf4951e30b
Joseph Pelletier <143644756+joey1pelletier@users.noreply.github.com>
2024-09-06 12:31:12 -0600

    yea

---
+++ temp-atls4630/script.js
@@ -0,0 +1,26 @@
+console.log("hello world");
+let helloWorldVariable = "hello world";
+const helloWorldConst = "hello world const";
+
+let stringWithQuotes = "this is 'quote' with strings";
+
+const myArray = ['cats','dogs','goats'];
+console.log(myArray);
+console.log(myArray[0]);
+
+console.log(helloWorldVariable);
+console.log(helloWorldConst);
+
+myArray.push('pigs');
+console.log(myArray);
+
+const thisClassroom = {
+    building: "ATLAS",
+    roomNumber: 104
+};
+
+console.log(thisClassroom);
+thisClassroom.newProperty = 'new value';
+console.log(thisClassroom);
+
+console.log(thisClassroom['building']);
